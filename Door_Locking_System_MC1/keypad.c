40fc6e4214def3918e1edd069d3f380d12573260
ahmezzats <ahm.ezzat.s@gmail.com>
2019-10-12 15:47:53 +0200

    Solve the keypad Null Character problem

--- Door_Locking_System_MC1/keypad.c
+++ Door_Locking_System_MC1/keypad.c
@@ -73,11 +73,11 @@ static uint8 KeyPad_4x3_adjustKeyNumber(uint8 button_number)
 	{
 		case 10: return '*'; // ASCII Code of =
 				 break;
-		case 11: return 0;
+		case 11: return '0';
 				 break;		
 		case 12: return '#'; // ASCII Code of +
 				 break;
-		default: return button_number;								 						
+		default: return '0' + button_number;
 	}
 } 
 
@@ -87,39 +87,39 @@ static uint8 KeyPad_4x4_adjustKeyNumber(uint8 button_number)
 {
 	switch(button_number)
 	{
-		case 1: return 7; 
+		case 1: return '7';
 				break;
-		case 2: return 8; 
+		case 2: return '8';
 				break;
-		case 3: return 9; 
+		case 3: return '9';
 				break;
 		case 4: return '%'; // ASCII Code of %
 				break;
-		case 5: return 4; 
+		case 5: return '4';
 				break;
-		case 6: return 5;
+		case 6: return '5';
 				break;
-		case 7: return 6; 
+		case 7: return '6';
 				break;
 		case 8: return '*'; /* ASCII Code of '*' */
 				break;		
-		case 9: return 1; 
+		case 9: return '1';
 				break;
-		case 10: return 2; 
+		case 10: return '2';
 				break;
-		case 11: return 3; 
+		case 11: return '3';
 				break;
 		case 12: return '-'; /* ASCII Code of '-' */
 				break;
 		case 13: return 13;  /* ASCII of Enter */
 				break;			
-		case 14: return 0; 
+		case 14: return '0';
 				break;
 		case 15: return '='; /* ASCII Code of '=' */
 				break;
 		case 16: return '+'; /* ASCII Code of '+' */
 				break;
-		default: return button_number;								 						
+		default: return '0' + button_number;
 	}
 } 
 
